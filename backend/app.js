7d1b32326e0844644e8c238eac9ffa5daa108cd9
chirayu <sethchirayu@gmail.com>
2025-08-30 16:57:56 +0530

    cors settings changed

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,9 @@ const io= connectToSocket(server)
 const PORT=process.env.PORT|| 8000;
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin:"*",
+    credentials: true
+    
 }));
 app.use(cookieParser());
 app.use(express.json({limit:"40kb"}))
